2e89a0f2ac65a126c632c83c506374108abfd226
iwabuchiken <iwabuchi.k.2010@gmail.com>
2012-09-01 12:01:59 +0900

    m
    1.0
    
    ---
    1. Up to: Set title

---
+++ src/ic/main/MainActv.java
@@ -0,0 +1,21 @@
+package ic.main;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActv extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+    	/********************************
+		 * 
+		 ********************************/
+    	
+        super.onCreate(savedInstanceState);
+        
+        setContentView(R.layout.main);
+        
+        this.setTitle(this.getClass().getName());
+        
+    }
+}
